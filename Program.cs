dc75d3c9e8b6a7fd4475091e9cca6eccca96bc9b
Andrei <constantinescu321@gmail.com>
2025-07-01 23:17:31 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+// var services = new ServiceCollection();
+//
+// services.AddTransient<ICustomerRepository, SqlCustomerRepository>();
+// services.AddTransient<CustomerService>();
+//
+// var provider = services.BuildServiceProvider();
+//
+// var service = provider.GetRequiredService<CustomerService>();
+//
+// service.Register("Andrei");
